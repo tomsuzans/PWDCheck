c29860525b138a7a0c329c972f1e8243f4ab7a71
tomsuzans <73278234+tomsuzans@users.noreply.github.com>
2020-11-13 16:23:25 +0200

    Initial commit

---
+++ PWDCheck.Designer.cs
@@ -0,0 +1,273 @@
+﻿namespace PWDCheck
+{
+    partial class PWDCheck
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbparole1 = new System.Windows.Forms.TextBox();
+            this.tbparole2 = new System.Windows.Forms.TextBox();
+            this.Poga_parbaudit = new System.Windows.Forms.Button();
+            this.lblpaole_1 = new System.Windows.Forms.Label();
+            this.lblchar_1 = new System.Windows.Forms.Label();
+            this.lblburti_1 = new System.Windows.Forms.Label();
+            this.lblcipari_1 = new System.Windows.Forms.Label();
+            this.lblhash_1 = new System.Windows.Forms.Label();
+            this.lblvertiba_1 = new System.Windows.Forms.Label();
+            this.lblparole_2 = new System.Windows.Forms.Label();
+            this.lblsimboli_2 = new System.Windows.Forms.Label();
+            this.lblburti_2 = new System.Windows.Forms.Label();
+            this.lblcipari_2 = new System.Windows.Forms.Label();
+            this.lblhash_2 = new System.Windows.Forms.Label();
+            this.lblvertiba_2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tbparole1
+            // 
+            this.tbparole1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.tbparole1.Location = new System.Drawing.Point(53, 30);
+            this.tbparole1.Margin = new System.Windows.Forms.Padding(4);
+            this.tbparole1.Name = "tbparole1";
+            this.tbparole1.Size = new System.Drawing.Size(332, 23);
+            this.tbparole1.TabIndex = 2;
+            this.tbparole1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // tbparole2
+            // 
+            this.tbparole2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.tbparole2.Location = new System.Drawing.Point(53, 288);
+            this.tbparole2.Margin = new System.Windows.Forms.Padding(4);
+            this.tbparole2.Name = "tbparole2";
+            this.tbparole2.Size = new System.Drawing.Size(332, 23);
+            this.tbparole2.TabIndex = 10;
+            // 
+            // Poga_parbaudit
+            // 
+            this.Poga_parbaudit.Location = new System.Drawing.Point(522, 460);
+            this.Poga_parbaudit.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Poga_parbaudit.Name = "Poga_parbaudit";
+            this.Poga_parbaudit.Size = new System.Drawing.Size(190, 53);
+            this.Poga_parbaudit.TabIndex = 0;
+            this.Poga_parbaudit.Text = "Parbaudit";
+            this.Poga_parbaudit.UseVisualStyleBackColor = true;
+            this.Poga_parbaudit.Click += new System.EventHandler(this.PogaParbaudit);
+            // 
+            // lblpaole_1
+            // 
+            this.lblpaole_1.AutoSize = true;
+            this.lblpaole_1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.lblpaole_1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblpaole_1.Location = new System.Drawing.Point(50, 235);
+            this.lblpaole_1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblpaole_1.Name = "lblpaole_1";
+            this.lblpaole_1.Size = new System.Drawing.Size(53, 17);
+            this.lblpaole_1.TabIndex = 17;
+            this.lblpaole_1.Text = "Parole:";
+            // 
+            // lblchar_1
+            // 
+            this.lblchar_1.AutoSize = true;
+            this.lblchar_1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.lblchar_1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblchar_1.Location = new System.Drawing.Point(50, 204);
+            this.lblchar_1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblchar_1.Name = "lblchar_1";
+            this.lblchar_1.Size = new System.Drawing.Size(42, 17);
+            this.lblchar_1.TabIndex = 16;
+            this.lblchar_1.Text = "Char:";
+            // 
+            // lblburti_1
+            // 
+            this.lblburti_1.AutoSize = true;
+            this.lblburti_1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.lblburti_1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblburti_1.Location = new System.Drawing.Point(50, 172);
+            this.lblburti_1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblburti_1.Name = "lblburti_1";
+            this.lblburti_1.Size = new System.Drawing.Size(41, 17);
+            this.lblburti_1.TabIndex = 15;
+            this.lblburti_1.Text = "Burti:";
+            // 
+            // lblcipari_1
+            // 
+            this.lblcipari_1.AutoSize = true;
+            this.lblcipari_1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.lblcipari_1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblcipari_1.Location = new System.Drawing.Point(50, 142);
+            this.lblcipari_1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblcipari_1.Name = "lblcipari_1";
+            this.lblcipari_1.Size = new System.Drawing.Size(48, 17);
+            this.lblcipari_1.TabIndex = 14;
+            this.lblcipari_1.Text = "Cipari:";
+            // 
+            // lblhash_1
+            // 
+            this.lblhash_1.AutoSize = true;
+            this.lblhash_1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.lblhash_1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblhash_1.Location = new System.Drawing.Point(50, 112);
+            this.lblhash_1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblhash_1.Name = "lblhash_1";
+            this.lblhash_1.Size = new System.Drawing.Size(45, 17);
+            this.lblhash_1.TabIndex = 13;
+            this.lblhash_1.Text = "Hash:";
+            this.lblhash_1.Click += new System.EventHandler(this.lblhash_1_Click);
+            // 
+            // lblvertiba_1
+            // 
+            this.lblvertiba_1.AutoSize = true;
+            this.lblvertiba_1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.lblvertiba_1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblvertiba_1.Location = new System.Drawing.Point(50, 82);
+            this.lblvertiba_1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblvertiba_1.Name = "lblvertiba_1";
+            this.lblvertiba_1.Size = new System.Drawing.Size(108, 17);
+            this.lblvertiba_1.TabIndex = 12;
+            this.lblvertiba_1.Text = "Ievadita vertiba:";
+            // 
+            // lblparole_2
+            // 
+            this.lblparole_2.AutoSize = true;
+            this.lblparole_2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.lblparole_2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblparole_2.Location = new System.Drawing.Point(50, 510);
+            this.lblparole_2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblparole_2.Name = "lblparole_2";
+            this.lblparole_2.Size = new System.Drawing.Size(53, 17);
+            this.lblparole_2.TabIndex = 23;
+            this.lblparole_2.Text = "Parole:";
+            // 
+            // lblsimboli_2
+            // 
+            this.lblsimboli_2.AutoSize = true;
+            this.lblsimboli_2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.lblsimboli_2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblsimboli_2.Location = new System.Drawing.Point(50, 478);
+            this.lblsimboli_2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblsimboli_2.Name = "lblsimboli_2";
+            this.lblsimboli_2.Size = new System.Drawing.Size(42, 17);
+            this.lblsimboli_2.TabIndex = 22;
+            this.lblsimboli_2.Text = "Char:";
+            // 
+            // lblburti_2
+            // 
+            this.lblburti_2.AutoSize = true;
+            this.lblburti_2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.lblburti_2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblburti_2.Location = new System.Drawing.Point(50, 448);
+            this.lblburti_2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblburti_2.Name = "lblburti_2";
+            this.lblburti_2.Size = new System.Drawing.Size(41, 17);
+            this.lblburti_2.TabIndex = 21;
+            this.lblburti_2.Text = "Burti:";
+            // 
+            // lblcipari_2
+            // 
+            this.lblcipari_2.AutoSize = true;
+            this.lblcipari_2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.lblcipari_2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblcipari_2.Location = new System.Drawing.Point(50, 418);
+            this.lblcipari_2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblcipari_2.Name = "lblcipari_2";
+            this.lblcipari_2.Size = new System.Drawing.Size(48, 17);
+            this.lblcipari_2.TabIndex = 20;
+            this.lblcipari_2.Text = "Cipari:";
+            // 
+            // lblhash_2
+            // 
+            this.lblhash_2.AutoSize = true;
+            this.lblhash_2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.lblhash_2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblhash_2.Location = new System.Drawing.Point(50, 386);
+            this.lblhash_2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblhash_2.Name = "lblhash_2";
+            this.lblhash_2.Size = new System.Drawing.Size(45, 17);
+            this.lblhash_2.TabIndex = 19;
+            this.lblhash_2.Text = "Hash:";
+            // 
+            // lblvertiba_2
+            // 
+            this.lblvertiba_2.AutoSize = true;
+            this.lblvertiba_2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.lblvertiba_2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblvertiba_2.Location = new System.Drawing.Point(50, 356);
+            this.lblvertiba_2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblvertiba_2.Name = "lblvertiba_2";
+            this.lblvertiba_2.Size = new System.Drawing.Size(108, 17);
+            this.lblvertiba_2.TabIndex = 18;
+            this.lblvertiba_2.Text = "Ievadita vertiba:";
+            // 
+            // PWDCheck
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(773, 554);
+            this.Controls.Add(this.lblparole_2);
+            this.Controls.Add(this.lblsimboli_2);
+            this.Controls.Add(this.lblburti_2);
+            this.Controls.Add(this.lblcipari_2);
+            this.Controls.Add(this.lblhash_2);
+            this.Controls.Add(this.lblvertiba_2);
+            this.Controls.Add(this.lblpaole_1);
+            this.Controls.Add(this.lblchar_1);
+            this.Controls.Add(this.lblburti_1);
+            this.Controls.Add(this.lblcipari_1);
+            this.Controls.Add(this.lblhash_1);
+            this.Controls.Add(this.lblvertiba_1);
+            this.Controls.Add(this.Poga_parbaudit);
+            this.Controls.Add(this.tbparole2);
+            this.Controls.Add(this.tbparole1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "PWDCheck";
+            this.Text = "Password check";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbparole1;
+        private System.Windows.Forms.TextBox tbparole2;
+        private System.Windows.Forms.Button Poga_parbaudit;
+        private System.Windows.Forms.Label lblpaole_1;
+        private System.Windows.Forms.Label lblchar_1;
+        private System.Windows.Forms.Label lblburti_1;
+        private System.Windows.Forms.Label lblcipari_1;
+        private System.Windows.Forms.Label lblhash_1;
+        private System.Windows.Forms.Label lblvertiba_1;
+        private System.Windows.Forms.Label lblparole_2;
+        private System.Windows.Forms.Label lblsimboli_2;
+        private System.Windows.Forms.Label lblburti_2;
+        private System.Windows.Forms.Label lblcipari_2;
+        private System.Windows.Forms.Label lblhash_2;
+        private System.Windows.Forms.Label lblvertiba_2;
+    }
+}
+
---
+++ PWDCheck.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PWDCheck
+{
+    public partial class PWDCheck : Form
+    {
+        public PWDCheck()
+        {
+            InitializeComponent();
+        }
+
+        private void PogaParbaudit(object sender, EventArgs e)
+        {
+            lblvertiba_1.Text = "Ievadita vertiba: " + tbparole1.Text;
+            lblhash_1.Text = "Hash: " + Convert.ToString(tbparole1.Text.GetHashCode());
+
+            int cipari = 0;
+            int simboli = 0;
+            int burti = 0;
+
+            try
+            {
+                for (int i = 0; i < tbparole1.Text.Length; i++)
+                {
+                    if (tbparole1.Text[i] >= '0' && tbparole1.Text[i] <= '9')
+                    {
+                        cipari++;
+                    }
+                    else if ((tbparole1.Text[i] >= 'A' && tbparole1.Text[i] <= 'Z') || (tbparole1.Text[i] >= 'a' && tbparole1.Text[i] <= 'z'))
+                    {
+                        burti++;
+                    }
+                    else
+                    {
+                        simboli++;
+                    }
+                }
+            }
+            catch
+            {
+
+
+            }
+            lblcipari_1.Text = "Cipari: " + cipari.ToString();
+            lblburti_1.Text = "Burti: " + burti.ToString();
+            lblchar_1.Text = "Char: " + simboli.ToString();
+            if ((cipari + burti + simboli >= 11) && (cipari > 1) && (simboli > 1) && (burti > 1))
+            {
+                lblpaole_1.Text = "Password OK";
+            }
+            else lblpaole_1.Text = "Password NOT OK";
+
+
+
+            lblvertiba_2.Text = "Ievadita vertiba: " + tbparole2.Text;
+            lblhash_2.Text = "Hash: " + Convert.ToString(tbparole2.Text.GetHashCode());
+
+            int cipari2 = 0;
+            int simboli2 = 0;
+            int burti2 = 0;
+
+            try
+            {
+                for (int i = 0; i < tbparole2.Text.Length; i++)
+                {
+                    if (tbparole2.Text[i] >= '0' && tbparole2.Text[i] <= '9')
+                    {
+                        cipari2++;
+                    }
+                    else if ((tbparole2.Text[i] >= 'A' && tbparole2.Text[i] <= 'Z') || (tbparole2.Text[i] >= 'a' && tbparole2.Text[i] <= 'z'))
+                    {
+                        burti2++;
+                    }
+                    else
+                    {
+                        simboli2++;
+                    }
+                }
+            }
+            catch
+            {
+
+            }
+            lblcipari_2.Text = "Cipari: " + cipari2.ToString();
+            lblburti_2.Text = "Burti: " + burti2.ToString();
+            lblsimboli_2.Text = "Char: " + simboli2.ToString();
+            if ((cipari2 + burti2 + simboli2 >= 11) && (cipari2 > 1) && (simboli2 > 1) && (burti2 > 1))
+            {
+                lblparole_2.Text = "Password OK";
+            }
+            else lblparole_2.Text = "Password NOT OK";
+
+
+
+        }
+
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblhash_1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+ }
